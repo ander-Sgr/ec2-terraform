aa4a3c587a0ad3186a5f8e4597c5fa297c2c2f72
ander <andersonbarreto2001@gmail.com>
2024-07-22 23:40:38 +0200

    config a modular security group

---
+++ modules/security_group/main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "http_and_ssh_access" {
+  name_prefix = var.name_secgroup
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port     = ingress.value.from_port
+      to_port       = ingress.value.to_port
+      protocol      = ingress.value.protocol
+      cidr_blocks   = ingress.value.cidr_blocks 
+    }
+  }
+  
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      from_port     = egress-value.from_port
+      to_port       = egress.value.to_port
+      protocol      = egress.value.protocol
+      cidr_blocks   = egress.value.cidr_blocks
+    }
+  }
+
+  tags = {
+    Name = var.name_secgroup
+  }
+}
---
+++ modules/security_group/variables.tf
@@ -0,0 +1,26 @@
+variable "name_secgroup" {
+  type        = string 
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string) 
+  })) 
+  
+  default = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
